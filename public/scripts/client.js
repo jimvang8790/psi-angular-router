b2e439b2d387856dd2c66986ad729d2afff6fd14
Millie Walsh <millie@primeacademy.io>
2017-05-12 11:47:19 -0500

    show color swapping views

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,30 @@
 console.log('test');
+
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider, $locationProvider) {
+  $routeProvider.when('/', {
+    template: '<div> This is the default </div>',
+    controller: 'DefaultController as defCtrl'
+  }).when('/blue', {
+    templateUrl: 'views/pages/blue.html',
+    controller: 'BlueController as blueCtrl'
+  }).when('/yellow', {
+    templateUrl: 'views/pages/yellow.html',
+    controller: 'YellowController as yellowCtrl'
+  }).otherwise('/');
+
+  $locationProvider.html5Mode(true);
+});
+
+myApp.controller('DefaultController', function() {
+  console.log('DefaultController loaded');
+});
+
+myApp.controller('BlueController', function() {
+  console.log('BlueController loaded');
+});
+
+myApp.controller('YellowController', function() {
+  console.log('YellowController loaded');
+});
--- server.js
+++ server.js
@@ -4,9 +4,13 @@ var path = require('path');
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res) {
+// This should be the last route
+// /* is wildcard will respond to all requests
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/views/index.html'));
 });
+
+// No server side routes that are the same as the client side routes
 
 app.listen(5098, function() {
   console.log('listenign 5098');
